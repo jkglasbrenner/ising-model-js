60b238d374a3d28705eb458a65c1bdab36756478
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-25 01:55:35 -0500

    Wrappers for properly calling and animating the Ising model sim

---
+++ visualization/animateSimulation.js
@@ -0,0 +1,25 @@
+// Use of toRecursiveAnim adapted from
+// https://tkowal.wordpress.com/2014/09/07/functional-javascript-passing-additional-arguments-to-callback/
+
+var animateHeatmap = require('./animateHeatmap');
+var monteCarloSweep = require('../simulation/monteCarloSweep');
+
+const toRecursiveAnim = function ( callback, grid, parameters, spinFlipAlgorithm,
+                                   rng ) {
+  return function (ts) {
+    callback( ts, grid, parameters, spinFlipAlgorithm, rng );
+  };
+};
+
+const simulation = function( timestamp, grid, parameters, spinFlipAlgorithm,
+                             rng ) {
+  monteCarloSweep( grid, parameters, spinFlipAlgorithm, rng );
+  animateHeatmap( grid, parameters );
+  requestAnimationFrame( toRecursiveAnim( simulation, grid, parameters,
+                                          spinFlipAlgorithm, rng ) );
+};
+
+module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
+  requestAnimationFrame( toRecursiveAnim( simulation, grid, parameters,
+                                          spinFlipAlgorithm, rng ) );
+};
