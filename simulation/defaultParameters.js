2590bc1ed7d548a78a51db6d1e2a3fff11c8f33f
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-25 01:42:26 -0500

    Default parameters for Ising simulation

---
+++ simulation/defaultParameters.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  const defaultParameters = {
+    showAnimation: true,
+    seed: Math.floor(Math.random() * Math.pow(2, 31)),
+    nsweeps: 1000,
+    n: 100,
+    temperature: 2,
+    colorscale: 'YlGnBu',
+    heatmapID: 'isingModel',
+    frameDelay: 0,
+    redrawFrames: true,
+    energy: 0.0,
+    magnetization: 0.0,
+    exchangeParameter: 1.0
+  };
+  defaultParameters.numberSites = Math.pow(defaultParameters.n, 2);
+
+  return defaultParameters;
+};
