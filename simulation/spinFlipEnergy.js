9ec1e3cd78b27fe2ecd89e1564fb88295b0122d4
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-25 01:45:41 -0500

    Change in energy for flipping an Ising spin The energy change calculation assumes nearest-neighbor interaction only

---
+++ simulation/spinFlipEnergy.js
@@ -0,0 +1,13 @@
+var neighbors = require('./neighbors');
+
+module.exports = function ( grid, i, j, parameters ) {
+  let indicesSiteNeighbors = neighbors( i, j, parameters.n );
+  let siteSpin = grid[ i ][ j ];
+  let sumNeighborSpins = 0.0;
+  for ( let index of indicesSiteNeighbors ) {
+    sumNeighborSpins += grid[ index[ 0 ] ][ index[ 1 ] ];
+  }
+  let deltaE = 2.0 * parameters.exchangeParameter * siteSpin * sumNeighborSpins;
+
+  return deltaE;
+};
