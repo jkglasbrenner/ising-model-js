666ab265716342a8a3706dd8900241ab2d1e5bc0
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-25 01:51:13 -0500

    One MC step is pick site, find spin-flip energy, then test for flip

---
+++ simulation/monteCarloStep.js
@@ -0,0 +1,13 @@
+var pickSite = require('./pickSite');
+var spinFlipEnergy = require('./spinFlipEnergy');
+
+module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
+  let ij = pickSite( parameters.n, rng );
+  let energyDifference = spinFlipEnergy( grid, ij[0], ij[1], parameters );
+  let isSpinFlip = spinFlipAlgorithm( energyDifference, parameters.temperature,
+                                      rng );
+  if ( isSpinFlip ) {
+    grid[ ij[ 0 ] ][ ij[ 1 ] ] *= -1;
+    parameters.energy += energyDifference;
+  }
+};
