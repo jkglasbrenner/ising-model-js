1e0175341b9d0041d4820df3882ec9d3ee40d618
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-25 01:44:56 -0500

    Use modulo operator to find neighbors in periodic conditions

---
+++ simulation/neighbors.js
@@ -0,0 +1,12 @@
+var mod = require('../math/mod');
+
+module.exports = function ( x, y, n ) {
+  let neighborhood = [
+    [ x, mod( y + 1, n ) ],
+    [ x, mod( y - 1, n ) ],
+    [ mod( x + 1, n ), y ],
+    [ mod( x - 1, n ), y ]
+  ];
+
+  return neighborhood;
+};
