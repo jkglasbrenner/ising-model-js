2ee8b8355b5a11c942791d7b3b05acb5aad97dd3
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-27 12:40:55 -0500

    Pass parameters map instead of just the tmperature value

--- simulation/monteCarloStep.js
+++ simulation/monteCarloStep.js
@@ -4,8 +4,7 @@ const spinFlipEnergy = require('./spinFlipEnergy');
 module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
   let ij = pickSite( parameters.n, rng );
   let energyDifference = spinFlipEnergy( grid, ij[0], ij[1], parameters );
-  let isSpinFlip = spinFlipAlgorithm( energyDifference, parameters.temperature,
-                                      rng );
+  let isSpinFlip = spinFlipAlgorithm( energyDifference, parameters, rng );
   if ( isSpinFlip ) {
     grid[ ij[ 0 ] ][ ij[ 1 ] ] *= -1;
     parameters.energy += energyDifference;
