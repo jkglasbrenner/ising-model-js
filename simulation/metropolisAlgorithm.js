4a91d418cf8f237e772160c02988246bc58c933b
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-27 12:43:30 -0500

    Get temperature from slider instead of parameters map

--- simulation/metropolisAlgorithm.js
+++ simulation/metropolisAlgorithm.js
@@ -1,11 +1,14 @@
-module.exports = function ( energyDifference, temperature, rng ) {
+const noUiSlider = require('nouislider');
+
+module.exports = function ( energyDifference, parameters, rng ) {
   let isSpinFlip;
   let isEnergyLower = energyDifference <= 0;
+  let temperature = document.getElementById('temperature');
 
   if ( isEnergyLower ) {
     isSpinFlip = true;
   } else {
-    let chanceSpinFlip = Math.exp(- energyDifference / temperature);
+    let chanceSpinFlip = Math.exp( - energyDifference / temperature.noUiSlider.get() );
     let randNumber = rng.floating( { min: 0, max: 1, fixed: 15 } );
     isSpinFlip = randNumber < chanceSpinFlip;
   }
