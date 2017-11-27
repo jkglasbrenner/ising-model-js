36e5cc32713c27b85630ed5d69c4281c30aea66a
James K. Glasbrenner <jglasbr2@gmu.edu>
2017-11-27 12:43:53 -0500

    Add Start/Pause functionality to the simulation

--- visualization/animateSimulation.js
+++ visualization/animateSimulation.js
@@ -4,6 +4,9 @@
 const animateHeatmap = require('./animateHeatmap');
 const monteCarloSweep = require('../simulation/monteCarloSweep');
 
+let requestID;
+let startPause = document.getElementById('startPauseButton');
+
 const toRecursiveAnim = function ( callback, grid, parameters, spinFlipAlgorithm,
                                    rng ) {
   return function (ts) {
@@ -13,10 +16,15 @@ const toRecursiveAnim = function ( callback, grid, parameters, spinFlipAlgorithm
 
 const simulation = function( timestamp, grid, parameters, spinFlipAlgorithm,
                              rng ) {
-  monteCarloSweep( grid, parameters, spinFlipAlgorithm, rng );
-  animateHeatmap( grid, parameters );
-  requestAnimationFrame( toRecursiveAnim( simulation, grid, parameters,
-                                          spinFlipAlgorithm, rng ) );
+  if (startPause.innerHTML === "Pause") {
+    monteCarloSweep( grid, parameters, spinFlipAlgorithm, rng );
+    animateHeatmap( grid, parameters );
+    requestAnimationFrame( toRecursiveAnim( simulation, grid, parameters,
+                                            spinFlipAlgorithm, rng ) );
+  } else {
+    requestAnimationFrame( toRecursiveAnim( simulation, grid, parameters,
+                                            spinFlipAlgorithm, rng ) );
+  }
 };
 
 module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
