4d071ceb12616c52caababcf1e58979af4815270
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-02-02 12:38:31 -0500

    Compute magnetization on grid initialization

--- simulation/initGrid.js
+++ simulation/initGrid.js
@@ -1,11 +1,13 @@
-module.exports = function ( n, rng ) {
+module.exports = function ( parameters, rng ) {
   let grid = [];
   let gridRow = [];
-  for ( let i = 0; i < n; i++ ) {
+  parameters.magnetization = 0;
+  for ( let i = 0; i < parameters.gridSize; i++ ) {
     gridRow = [];
-    for ( let j = 0; j < n; j++ ) {
+    for ( let j = 0; j < parameters.gridSize; j++ ) {
       let spinDirection = rng.pickone( [ -1, 1 ] );
       gridRow.push( spinDirection );
+      parameters.magnetization += spinDirection;
     }
     grid.push( gridRow );
   }
