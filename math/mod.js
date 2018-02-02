62e24fb79f1a2c42cc00c11722224ea556ee6cd4
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-02-02 12:37:44 -0500

    Fixed algorithm for computing modulus

--- math/mod.js
+++ math/mod.js
@@ -1,3 +1,7 @@
 module.exports = function ( n, m ) {
-  return ( ( n % m ) + m ) % m;
+  let remainder = n % m;
+  let shifted = Number(remainder) + Number(m);
+  let trueMod = shifted % m;
+  return trueMod;
+  // return ( ( n % m ) + m ) % m;
 };
