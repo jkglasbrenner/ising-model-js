c75474e6942f386a1ca3d7014cda498ee2ec18b9
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-02-02 12:43:22 -0500

    Add burn-in period; Compute running average of magnetization

--- simulation/monteCarloSweep.js
+++ simulation/monteCarloSweep.js
@@ -4,11 +4,18 @@ const spinFlipEnergy = require('./spinFlipEnergy');
 
 module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
   let numberSweeps = document.getElementById('numberSweeps');
+  let averageMagnetization = document.getElementById('averageMagnetization');
 
   for (let step = 0; step < parameters.numberSites; step++) {
     monteCarloStep( grid, parameters, spinFlipAlgorithm, rng );
   }
 
-  parameters.sweepNumber += 1;
-  numberSweeps.innerText = parameters.sweepNumber;
+  let nSweeps = Number(numberSweeps.innerText) + 1;
+  if ( nSweeps > parameters.burnInSteps ) {
+    let avgMag = Number(averageMagnetization.innerText) + (parameters.magnetization * 1.0 - Number(averageMagnetization.innerText)) / Number(nSweeps - parameters.burnInSteps);
+    averageMagnetization.innerText = avgMag;
+  } else {
+    averageMagnetization.innerText = parameters.magnetization;
+  }
+  numberSweeps.innerText = nSweeps;
 };
