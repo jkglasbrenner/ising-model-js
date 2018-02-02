35d8650ab1b3a90592eb616ba04473922820ab79
James K. Glasbrenner <jglasbr2@gmu.edu>
2018-02-02 12:42:32 -0500

    Rename variable in parameters map from n to gridSize

--- simulation/monteCarloStep.js
+++ simulation/monteCarloStep.js
@@ -2,7 +2,7 @@ const pickSite = require('./pickSite');
 const spinFlipEnergy = require('./spinFlipEnergy');
 
 module.exports = function ( grid, parameters, spinFlipAlgorithm, rng ) {
-  let ij = pickSite( parameters.n, rng );
+  let ij = pickSite( parameters.gridSize, rng );
   let energyDifference = spinFlipEnergy( grid, ij[0], ij[1], parameters );
   let isSpinFlip = spinFlipAlgorithm( energyDifference, parameters, rng );
   if ( isSpinFlip ) {
